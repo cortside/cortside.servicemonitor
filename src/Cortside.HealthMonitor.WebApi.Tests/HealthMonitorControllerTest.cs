9c889d7bb42c0bf2947660532e2836fdb7b5828d
Cort Schaefer <cort@eschaefer.net>
2023-11-21 18:42:29 -0700

    updated docker build and remove empty test project

--- src/Cortside.HealthMonitor.WebApi.Tests/HealthMonitorControllerTest.cs
+++
@@ -1,8 +0,0 @@
-using Cortside.HealthMonitor.WebApi.Controllers;
-
-namespace Cortside.HealthMonitor.WebApi.Tests {
-    public class ServicesControllerTest : ControllerTest<ServicesController> {
-        public ServicesControllerTest() : base() {
-        }
-    }
-}
